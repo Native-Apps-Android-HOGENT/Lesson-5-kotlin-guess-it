ffd63515b7a400e0602eb51eb6bce548b05563d5
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-11-06 10:55:41 +0100

    fix: use JSON attribute name for correct Moshi parsing

--- app/src/main/java/com/example/android/guesstheword/domain/Word.kt
+++ app/src/main/java/com/example/android/guesstheword/domain/Word.kt
@@ -2,9 +2,11 @@ package com.example.android.guesstheword.domain
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.squareup.moshi.Json
 
 @Entity(tableName = "word_table")
 data class Word(
+        @Json(name = "description")
         val word: String,
         @PrimaryKey(autoGenerate = true)
         var id: Long = 0L
