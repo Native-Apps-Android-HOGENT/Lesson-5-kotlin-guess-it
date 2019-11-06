b04d7d9c90c170efad8d4b559aab8ce2ce47191f
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-11-06 10:56:35 +0100

    fix: allow overwriting of DB entries instead of crashing

--- app/src/main/java/com/example/android/guesstheword/database/WordDao.kt
+++ app/src/main/java/com/example/android/guesstheword/database/WordDao.kt
@@ -2,13 +2,15 @@ package com.example.android.guesstheword.database
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.android.guesstheword.domain.Word
 
 @Dao
 interface WordDao {
 
-    @Insert
+    // When not specified Room will crash when trying to enter a Word twice.
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(word: Word)
 
     @Query("SELECT * FROM word_table")
