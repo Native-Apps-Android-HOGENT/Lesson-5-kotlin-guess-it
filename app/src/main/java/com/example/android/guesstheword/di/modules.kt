686db8b6ce2dada381d8809b1636ae5864ebe701
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-12-02 16:55:57 +0100

    refactor: move building of WordApiService to Koin Module

--- app/src/main/java/com/example/android/guesstheword/di/modules.kt
+++ app/src/main/java/com/example/android/guesstheword/di/modules.kt
@@ -4,18 +4,39 @@ import android.content.Context
 import android.net.ConnectivityManager
 import com.example.android.guesstheword.database.WordDatabase
 import com.example.android.guesstheword.domain.WordRepository
-import com.example.android.guesstheword.network.WordApi
+import com.example.android.guesstheword.network.BASE_URL
+import com.example.android.guesstheword.network.WordApiService
 import com.example.android.guesstheword.screens.game.GameViewModel
 import com.example.android.guesstheword.screens.title.TitleViewModel
 import com.example.android.guesstheword.screens.wordoverview.WordListViewModel
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 val mainModule = module {
 
+    single {
+        Moshi.Builder()
+                .add(KotlinJsonAdapterFactory())
+                .build() as Moshi
+    }
+
+    single {
+        Retrofit.Builder()
+                .addConverterFactory(MoshiConverterFactory.create(get()))
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .baseUrl(BASE_URL)
+                .build() as Retrofit
+    }
+
+    single { provideWordApiService(get()) }
+
     single { WordDatabase.getInstance(get()).wordDao }
-    single { WordApi.retrofitService }
 
     single { androidContext().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager }
 
@@ -24,4 +45,8 @@ val mainModule = module {
     viewModel { GameViewModel(get()) }
     viewModel { TitleViewModel(get()) }
     viewModel { WordListViewModel(get()) }
+}
+
+private fun provideWordApiService(retrofit: Retrofit): WordApiService {
+    return retrofit.create(WordApiService::class.java)
 }
--- app/src/main/java/com/example/android/guesstheword/network/WordApiService.kt
+++ app/src/main/java/com/example/android/guesstheword/network/WordApiService.kt
@@ -2,24 +2,9 @@ package com.example.android.guesstheword.network
 
 
 import com.example.android.guesstheword.domain.Word
-import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
-private const val BASE_URL = "http://giveaword.e-quality.be/"
-
-private val moshi = Moshi.Builder()
-        .add(KotlinJsonAdapterFactory())
-        .build()
-
-private val retrofit = Retrofit.Builder()
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
-        .addCallAdapterFactory(CoroutineCallAdapterFactory())
-        .baseUrl(BASE_URL)
-        .build()
+const val BASE_URL = "http://giveaword.e-quality.be/"
 
 interface WordApiService {
 
@@ -27,8 +12,3 @@ interface WordApiService {
     suspend fun getWords(): List<Word>
 }
 
-object WordApi {
-    val retrofitService: WordApiService by lazy {
-        retrofit.create(WordApiService::class.java)
-    }
-}
